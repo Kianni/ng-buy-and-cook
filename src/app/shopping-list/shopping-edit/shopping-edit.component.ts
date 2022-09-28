9fe80d7efd86b0872563eececfb0de7ff74ea5ed
Kirill <kirill.nikolaev@everon.fi>
2022-09-28 15:32:52 +0300

    allowing the selection items in the list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -8,17 +9,30 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
   newIngredient: Ingredient;
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.subscription = this.shoppingListService.startedEditing.subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.editedItemIndex = index;
+      }
+    );
   }
 
   onAddItem(form: NgForm) {
     const value = form.value;
     this.newIngredient = new Ingredient(value.name, value.amount)
     this.shoppingListService.addIngredient(this.newIngredient);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,30 +5,31 @@ import { Ingredient } from "../shared/ingredient.model";
 
 // @Injectable()
 export class ShoppingListService {
-    ingredientsChanged = new Subject<Ingredient[]>();
-    private ingredients = [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomatoes', 10)
-      ]
+  ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
+  private ingredients = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ]
 
-    // constructor(private recipeService: RecipeService){}
+  // constructor(private recipeService: RecipeService){}
 
-    getIngredients() {
-        return this.ingredients.slice();
-    }
+  getIngredients() {
+    return this.ingredients.slice();
+  }
 
-      addIngredient(item: Ingredient) {
-        this.ingredients.push(item);
-        this.ingredientsChanged.next(this.ingredients.slice())
-      }
+  addIngredient(item: Ingredient) {
+    this.ingredients.push(item);
+    this.ingredientsChanged.next(this.ingredients.slice())
+  }
 
-      addIngredients(ingredients: Ingredient[]){
-      //   for (let ingredient of ingredients) {
-      //     this.addIngredient(ingredient);
-      // }
-      this.ingredients.push(...ingredients);
-      this.ingredientsChanged.next(this.ingredients.slice())
-    }
+  addIngredients(ingredients: Ingredient[]) {
+    //   for (let ingredient of ingredients) {
+    //     this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice())
+  }
 
-    
+
 }
