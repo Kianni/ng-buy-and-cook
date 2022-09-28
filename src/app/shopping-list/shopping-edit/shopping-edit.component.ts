1b2bfb692a7e0029f2498802c911b2bb6b09461d
Kirill <kirill.nikolaev@everon.fi>
2022-09-28 15:51:18 +0300

    updating existing items

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -36,7 +36,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm) {
     const value = form.value;
     this.newIngredient = new Ingredient(value.name, value.amount)
-    this.shoppingListService.addIngredient(this.newIngredient);
+    if (this.editMode) {
+      this.shoppingListService.updateIngredient(this.editedItemIndex, this.newIngredient)
+    } else {
+      this.shoppingListService.addIngredient(this.newIngredient);
+    }
   }
 
   ngOnDestroy(): void {
